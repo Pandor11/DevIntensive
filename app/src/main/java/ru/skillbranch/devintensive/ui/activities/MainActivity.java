ffe88c916d247ed038e9ad60a7182f47a75b7d99
pandor11 <pandor11@yandex.ru>
2019-12-01 20:22:33 +0300

    added dimens, colors

--- app/src/main/java/ru/skillbranch/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/ru/skillbranch/devintensive/ui/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package ru.skillbranch.devintensive;
+package ru.skillbranch.devintensive.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import ru.skillbranch.devintensive.R;
 
 public class MainActivity extends AppCompatActivity {
 
