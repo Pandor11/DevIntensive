0a73baacc008ad6f3a6a3db6a8d8b61b16065089
pandor11 <pandor11@yandex.ru>
2020-01-13 22:27:56 +0300

    for Elena EelenA

--- app/src/main/java/ru/skillbranch/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/ru/skillbranch/devintensive/ui/activities/MainActivity.java
@@ -1,16 +1,70 @@
 package ru.skillbranch.devintensive.ui.activities;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import ru.skillbranch.devintensive.R;
+import ru.skillbranch.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected EditText mEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+        } else {
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/ConstantManager.java
@@ -0,0 +1,5 @@
+package ru.skillbranch.devintensive.utils;
+
+public interface ConstantManager {
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+}
